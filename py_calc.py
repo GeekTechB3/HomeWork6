73e535b5a4e9a9220fca2b757af3f181198a1b6f
kylych1988 <kylychsarykov@gmail.com>
2022-12-26 20:34:35 +0600

    pore

--- py_calc.py
+++ py_calc.py
@@ -11,41 +11,61 @@ class Form(QDialog):
         self.sub.clicked.connect(self.sub_num)
         self.mult.clicked.connect(self.mult_num)
         self.div.clicked.connect(self.div_num)
+        self.degree.clicked.connect(self.degree_num)
+        self.modul.clicked.connect(self.modul_num)
 
     def add_num(self):
         try:
             num1 = int(self.input1.text())
             num2 = int(self.input2.text())
-            self.output.setText("Ответ:"+str(num1 + num2))
+            self.output.display(str(num1 + num2))
         except ValueError:
-            self.output.setText("Не пиши буквы!")
+            self.error.setText("Не пиши буквы!")
 
     def sub_num(self):
         try:
             num1 = int(self.input1.text())
             num2 = int(self.input2.text())
-            self.output.setText("Ответ"+str(num1 - num2))
+            self.output.display(str(num1 - num2))
         except ValueError:
-            self.output.setText("Не пиши буквы!")
+            self.error.setText("Не пиши буквы!")
 
     def mult_num(self):
         try:
             num1 = int(self.input1.text())
             num2 = int(self.input2.text())
-            self.output.setText("Ответ:"+str(num1 * num2))
+            self.output.display(str(num1 * num2))
         except ValueError:
-            self.output.setText("Не пиши буквы!")
+            self.error.setText("Не пиши буквы!")
 
     def div_num(self):
         try:
-            try:
-                num1 = int(self.input1.text())
-                num2 = int(self.input2.text())
-                self.output.setText("Ответ:"+str(num1 / num2))
-            except ValueError:
-                self.output.setText("Не пиши буквы!")
+
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.output.display(str(num1 / num2))
+        except ValueError:
+            self.error.setText("Не пиши буквы!")
         except ZeroDivisionError:
                 self.output.setText("На ноль делить нельзя!")
+
+    def degree_num(self):
+        try:
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.output.display(num1 ** num2)
+        except ValueError:
+            self.error.setText("Не пиши буквы!")
+
+    def modul_num(self):
+        try:
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.output.display(num1 % num2)
+        except ValueError:
+            self.error.setText("Не пиши буквы!")
+
+
 
 app = QApplication(sys.argv)
 form = Form()
